de8c65c6e9c01c16eff0240d6d14a303e3e9a9bf
hehanpeng <287737281@qq.com>
2021-06-01 08:54:28 +0800

    2021-05-31 13:40:29

--- waf-srv/model/ttoInfo.go
+++ waf-srv/model/ttoInfo.go
@@ -14,7 +14,7 @@ type TtoInfo struct {
 	TtoType      string    `json:"ttoType" form:"ttoType" gorm:"column:tto_type;comment:类型（全局/局部）0:全局 1：局部;type:varchar(1);size:1;"`
 	TtoStatus    string    `json:"ttoStatus" form:"ttoStatus" gorm:"column:tto_status;comment:STATUS状态 0：未执行 1：已执行;type:varchar(1);size:1;"`
 	BizType      string    `json:"bizType" form:"bizType" gorm:"column:biz_type;comment:业务类型;type:varchar(64);size:64;"`
-	CallIp       string    `json:"callIp" form:"callIp" gorm:"column:call_ip;comment:回调ip;type:varchar(64);size:64;"`
+	CallSrvName  string    `json:"callSrvName" form:"callSrvName" gorm:"column:call_srv_name;comment:回调服务名;type:varchar(64);size:64;"`
 	CallMethod   string    `json:"callMethod" form:"callMethod" gorm:"column:call_method;comment:回调方法;type:varchar(128);size:128;"`
 	ExecuteTime  time.Time `json:"executeTime" form:"executeTime" gorm:"column:execute_time;comment:执行时间;type:datetime"`
 	ExpiredTime  int       `json:"expiredTime" form:"expiredTime" gorm:"column:expired_time;comment:过期时间,单位毫秒;type:bigint;size:19;"`
