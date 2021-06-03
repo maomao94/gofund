2f8ff4996193833facb092e3935f16bd272e6016
hehanpeng <287737281@qq.com>
2021-06-03 11:19:03 +0800

    增加注册接口

--- waf-srv/api/v1/test.go
+++ waf-srv/api/v1/test.go
@@ -57,6 +57,6 @@ func HelloLock(c *gin.Context) {
 func Hello(c *gin.Context) {
 	var ttoInfo model.TtoInfo
 	_ = c.ShouldBindJSON(&ttoInfo)
-	time.Sleep(1900 * time.Millisecond)
+	//time.Sleep(1900 * time.Millisecond)
 	api.Ok(c)
 }
